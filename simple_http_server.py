8345bd1240b13cb70210fd9f58fa03ee8d7fbb75
Cychih <michael66230@gmail.com>
2015-01-25 00:46:12 +0800

    print a message when requests finished

--- simple_http_server.py
+++ simple_http_server.py
@@ -5,30 +5,36 @@ import sys
 VERSION = sys.version_info[0]
 
 if VERSION == 3:
-    import http.server
-    import socketserver
+    import http.server as HttpServerModule
+    import socketserver as SocketServerModule
 else:
-    import SimpleHTTPServer
-    import SocketServer
+    import SimpleHTTPServer as HttpServerModule
+    import SocketServer as SocketServerModule
 
 import show_my_ip
-
 
 PORT = 8000 if len(sys.argv) == 1 else int(sys.argv[1])
 
 show_my_ip.output()
 
-if VERSION == 3:
-    server_address = ('', PORT)
-    Handler = http.server.SimpleHTTPRequestHandler
-    httpd = socketserver.TCPServer(("", PORT), Handler)
-else:
-    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-    httpd = SocketServer.TCPServer(("", PORT), Handler)
+Handler = HttpServerModule.SimpleHTTPRequestHandler
+class myHandler (Handler):
+
+    def do_GET (self):
+        super(myHandler, self).do_GET()
+        print("Request ends")
+
+    def do_HEAD (self):
+        super(myHandler, self).do_HEAD()
+        print("Request ends")
+
+# httpd = SocketServerModule.TCPServer(("", PORT), Handler)
+httpd = SocketServerModule.TCPServer(("", PORT), myHandler)
 
 try:
     print('Serving at port: %d'%(PORT) )
     httpd.serve_forever()
 except KeyboardInterrupt:
     print('KeyboardInterrupt detected, exit.')
+    httpd.shutdown()
     exit()
