02fe37231c792c234bfdc1d6a4421b32274699bb
Cychih <michael66230@gmail.com>
2016-02-01 05:17:36 +0800

    add progress bar, thanks for  wdv4758h/http.upload

--- static/main.js
+++ static/main.js
@@ -15,9 +15,70 @@ $(function () {
 
     show_qr_code(window.location);
 
+    $('#file').change(file_selected);
+    $('#btn_upload').click(file_upload);
+
 });
+
 
 function show_qr_code (text) {
     qrcode.clear();
     qrcode.makeCode(decodeURIComponent(text));
+}
+
+
+function file_selected () {
+    var file = $('#file')[0].files[0];
+
+    $('#progress-bar-container').css('display', 'block');
+    $('#file-size').text(file.size);
+}
+
+
+function file_upload () {
+    var req = new XMLHttpRequest();
+    var form = document.getElementById('form');
+    var data = new FormData(form);
+    var url = form.action;
+
+    req.upload.addEventListener('progress', show_upload_progress, false);
+    req.addEventListener('load', show_upload_complete_message, false);
+    req.addEventListener('error', show_upload_fail_message, false);
+    req.addEventListener('abort', show_upload_cancel_message, false);
+    req.open('POST', url, true);
+    req.send(data);
+
+    return false;
+}
+
+
+function show_upload_progress (evt) {
+    if (evt.lengthComputable) {
+        var percent = Math.round(evt.loaded * 100 / evt.total);
+        $('#progress-bar').css('width', percent +'%');
+        $('#progress-bar').text(percent +'%');
+        $('#file-size').text(evt.loaded +'/'+ evt.total);
+    } else {
+        $('#progress-bar').css({
+            'width': '100%',
+            'background': 'yellow',
+        });
+        $('#progress-bar').text('Unable to compute');
+    }
+}
+
+
+function show_upload_complete_message () {
+    $('#message').text('Upload succeed.');
+    window.location.reload();
+}
+
+
+function show_upload_fail_message () {
+    $('#message').text('Upload failed.');
+}
+
+
+function show_upload_cancel_message () {
+    $('#message').text('Upload canceled.');
 }
