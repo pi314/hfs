7ca6066f00453be92b45f6fe84585fcfbb9427c2
Cychih <michael66230@gmail.com>
2016-07-06 03:43:08 +0800

    use table to contain pending files list

--- hfs/static/main.js
+++ hfs/static/main.js
@@ -2,12 +2,10 @@ var metadata = [
     /* structure: */
     /*
     {
-         file: <file>,
-         form:
-         li:
-         progress_bar:
-         progress:
-         fsize:
+         file: file,
+         form: form,
+         progress_bar: div_progress_bar;
+         progress_ratio: td_fsize;
     }
     */
 ];
@@ -58,33 +56,21 @@ function add_progress_bar (metadata_obj) {
 
     var tr = $('<tr>')
     var td_fname = $('<td class="fname">'+ metadata_obj.file.name +'</td>')
+    var div_progress_bar = $('<div class="progress-bar">');
     var td_fsize = $('<td class="fsize">'+ metadata_obj.file.size +'</td>')
+    var td_form = $('<td>');
+    var form = $('<form method="POST" action="/'+ curdir +'" enctype="multipart/form-data">');
+    td_fname.append(div_progress_bar);
     tr.append(td_fname);
     tr.append(td_fsize);
+    td_form.append(form);
+    tr.append(td_form);
 
-    // var form = $('<form method="POST" action="/'+ curdir +'" enctype="multipart/form-data">');
-    // var li = $('<li id="'+ metadata_obj.id +'"></li>');
-    // var progress_bar = $('<div class="progress-bar">');
-    // var progress = $('<div class="progress">');
-    // var filesize = $('<div class="file-size">'+ metadata_obj.file.size +'</div>')
-    // var file = $('<input type="file" name="upload" class="hidden">');
-
-    // console.log(form);
-    //
-    // li.append(form);
-    // form.append(file);
-    // form.append(progress_bar);
-    // progress_bar.append(progress);
-    // progress_bar.append('<div class="file-name">'+ metadata_obj.file.name +'</div>');
-    // progress_bar.append(filesize);
-    // $('#pending_files').append(li);
     $('#pending-files').append(tr);
 
-    // metadata_obj.form = form;
-    // metadata_obj.li = li;
-    // metadata_obj.progress_bar = progress_bar;
-    // metadata_obj.progress = progress;
-    // metadata_obj.filesize = filesize;
+    metadata_obj.form = form;
+    metadata_obj.progress_bar = div_progress_bar;
+    metadata_obj.progress_ratio = td_fsize;
 }
 
 
@@ -100,6 +86,41 @@ function on_file_selected () {
 }
 
 
+function show_upload_progress (metadata_obj, evt) {
+    if (evt.lengthComputable) {
+        var percent = Math.round(evt.loaded * 100 / evt.total);
+        metadata_obj.progress_bar.css('width', percent +'%');
+        metadata_obj.progress_ratio.text(evt.loaded +'/'+ evt.total);
+    } else {
+        metadata_obj.progress_bar.css({
+            'width': '100%',
+            'background': 'yellow',
+        });
+        metadata_obj.progress_ratio.text(evt.loaded +'/?');
+    }
+}
+
+
+function show_upload_complete_message (index) {
+    $('#message').text('Upload succeed.');
+    if (index == metadata.length - 1) {
+        window.location.reload();
+    } else {
+        upload_file(index + 1);
+    }
+}
+
+
+function show_upload_fail_message () {
+    $('#message').text('Upload failed.');
+}
+
+
+function show_upload_cancel_message () {
+    $('#message').text('Upload canceled.');
+}
+
+
 function upload_file (index) {
     if (index >= metadata.length) {
         return;
@@ -112,42 +133,13 @@ function upload_file (index) {
 
     form.append('upload', metadata_obj.file);
 
-    function show_upload_progress (evt) {
-        if (evt.lengthComputable) {
-            var percent = Math.round(evt.loaded * 100 / evt.total);
-            metadata_obj.progress.css('width', percent +'%');
-            metadata_obj.fsize.text(evt.loaded +'/'+ evt.total);
-        } else {
-            metadata_obj.progress.css({
-                'width': '100%',
-                'background': 'yellow',
-            });
-            metadata_obj.fsize.text('N/A');
-        }
-    }
 
-
-    function show_upload_complete_message () {
-        $('#message').text('Upload succeed.');
-        if (index == metadata.length - 1) {
-            window.location.reload();
-        } else {
-            upload_file(index + 1);
-        }
-    }
-
-
-    function show_upload_fail_message () {
-        $('#message').text('Upload failed.');
-    }
-
-
-    function show_upload_cancel_message () {
-        $('#message').text('Upload canceled.');
-    }
-
-    req.upload.addEventListener('progress', show_upload_progress, false);
-    req.addEventListener('load', show_upload_complete_message, false);
+    req.upload.addEventListener('progress', function (evt) {
+        show_upload_progress(metadata_obj, evt);
+    }, false);
+    req.addEventListener('load', function () {
+        show_upload_complete_message(index);
+    }, false);
     req.addEventListener('error', show_upload_fail_message, false);
     // req.addEventListener('abort', show_upload_cancel_message, false);
     req.open(method, url, true);
